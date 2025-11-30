111d02ec19163fac3b5afccb1c244bf6553ff23c
Iván Villanueva Sabalete <villanueva_i1@hotmail.com>
2025-11-30 20:22:11 +0100

    Change Twitter icon in helper.R
    Updated Twitter icon HTML for Font Awesome version.

--- R/helper.R
+++ R/helper.R
@@ -19,7 +19,7 @@ if (!dir.exists("png")) dir.create("png")
 
 
 # Información del autor para el pie de gráfico
-twitter <- "<span style='color:#000;font-family: \"Font Awesome 6 Brands\"'>&#xf081;</span>"
+twitter <- "<span style='color:#000;font-family: \"Font Awesome 6 Brands\"'>&#xe61a;</span>"
 tweetelcheff <- "<span>*@elcheff*</span>"
 insta <- "<span style='color:#E1306C;font-family: \"Font Awesome 6 Brands\"'>&#xE055;</span>"
 instaelcheff <- "<span>*@sport_iv0*</span>"
